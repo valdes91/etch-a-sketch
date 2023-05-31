93c6476e6010332f1efb7121e27a1e87901976d2
Leonardo Valdes <leon_valdes7591@yahoo.com>
2023-05-31 16:08:46 -0700

    add basic cell coloring feature

--- main.js
+++ main.js
@@ -13,4 +13,19 @@ function setUpGrid() {
 	}
 }
 
+function colorCell(e) {
+	//checking to see if we are clicking on a cell
+	if (e.target.classList.contains('cell') && e.buttons === 1) {
+		console.log('youre drawing');
+		e.target.style.background = 'black';
+	}
+}
+
+function setupColorListeners() {
+	grid.addEventListener('mousedown', colorCell);
+
+	grid.addEventListener('mouseover', colorCell);
+}
+
 setUpGrid();
+setupColorListeners();
